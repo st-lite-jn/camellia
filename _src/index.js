0dc8efdb49fbfa6c7f8ccd1ba16abfa888b1eded
jey <diolite.jn@gmail.com>
2022-12-05 00:17:08 +0900

    キャッシュ削除

--- _src/index.js
+++
@@ -1,3 +0,0 @@
-import "./js/main.js";
-import "./js/block.js";
-import "./scss/style.scss";
--- functions/resister-block-styles/cover.php
+++
@@ -1,9 +0,0 @@
-<?php
-register_block_style(
-	'core/cover',
-	[
-		'name'			=> 'container-over',
-		'label'			=> __( '画面幅いっぱい', 'tsubaki' ),
-		'style_handle'	=> 'editor',
-	]
-);
